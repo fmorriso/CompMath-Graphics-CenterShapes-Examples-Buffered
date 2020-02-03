31e73940a2e4200e4bec61720aa80418992e0af5
Fred Morrison <fpmorrison@verizon.net>
2020-02-03 13:59:32 -0500

    make redrawBuffer method flexible

--- CenterShapesBufferedDriver.java
+++ CenterShapesBufferedDriver.java
@@ -4,23 +4,22 @@ import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
-public class CenterShapesBufferedDriver
-{
-	public static void main(String[] args)
-	{
-		JFrame frame = new JFrame("Center Shapes Buffered");
-		
+public class CenterShapesBufferedDriver {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Center Shapes - Buffered");
+
 		// use a portion of the available device screen
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		final double pct = 2.0/3;
-		final int squareSize = (int) (Math.min(screenSize.height, screenSize.width) * pct);
-		frame.setSize(squareSize, squareSize);
-		
+		final double pct = 2.0 / 3;
+		final int squareSize = (int) (Math.min(screenSize.height, screenSize.width) * pct) * 100 / 100;
+		final Dimension frameSize = new Dimension(squareSize, squareSize);
+		frame.setSize(frameSize);
+
 		// center the JFrame on the current device
-		frame.setLocationRelativeTo(null); 
-		
+		frame.setLocationRelativeTo(null);
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setContentPane(new CenterShapesBufferedPanel());
+		frame.setContentPane(new CenterShapesBufferedPanel(frameSize));
 		frame.setVisible(true);
 	}
 
--- CenterShapesBufferedPanel.java
+++ CenterShapesBufferedPanel.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
@@ -12,31 +13,31 @@ import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class CenterShapesBufferedPanel extends JPanel
-{
-	private static final int MIN_WIDTH = 100, MIN_HEIGHT = 100;
+@SuppressWarnings("serial")
+public class CenterShapesBufferedPanel extends JPanel {
+	private static int MIN_WIDTH = 100, MIN_HEIGHT = 100;
 
 	private BufferedImage bufImage;
 	private Graphics bufGraphics;
 
-	public CenterShapesBufferedPanel()
-	{
+	public CenterShapesBufferedPanel(Dimension parentSize) {
 
+		if (parentSize != null && parentSize.getHeight() != 0) {
+			MIN_WIDTH = parentSize.width;
+			MIN_HEIGHT = parentSize.height;
+		}
 		bufImage = new BufferedImage(MIN_WIDTH, MIN_HEIGHT, BufferedImage.TYPE_INT_RGB);
 		bufGraphics = bufImage.getGraphics();
 
 		// listen for when this JPanel is resized
-		this.addComponentListener(new ComponentAdapter()
-		{
+		this.addComponentListener(new ComponentAdapter() {
 			@Override
-			public void componentResized(ComponentEvent e)
-			{
-//				 System.out.println("componentResized");
+			public void componentResized(ComponentEvent e) {
+				// System.out.println("componentResized");
 
 				// set our size to be the same as our parent container
 				Component comp = e.getComponent();
-				if (comp instanceof JPanel)
-				{
+				if (comp instanceof JPanel) {
 					JPanel pnl = (JPanel) comp;
 					// get current size of parent container (because it may have been resized)
 					Container parent = pnl.getParent();
@@ -51,18 +52,20 @@ public class CenterShapesBufferedPanel extends JPanel
 
 	}
 
-	private void redrawBuffer()
-	{
+	private void redrawBuffer() {
+		int x, y, width, height;
 		Container parent = getParent();
-		if (parent == null)
-		{
-			// System.out.println("redrawBuffer: getParent() returned null");
-			return;
+		if (parent == null) {
+			x = 0;
+			y = 0;
+			width = MIN_WIDTH;
+			height = MIN_HEIGHT;
+		} else {			
+		 x = parent.getX();
+		 y = parent.getY();
+		 width = parent.getWidth();
+		 height = parent.getHeight();
 		}
-		int x = parent.getX();
-		int y = parent.getY();
-		int width = parent.getWidth();
-		int height = parent.getHeight();
 
 		bufImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		bufGraphics = bufImage.createGraphics();
@@ -83,8 +86,7 @@ public class CenterShapesBufferedPanel extends JPanel
 	 * @param g - the Graphics instance we are drawing on
 	 */
 	@Override
-	protected void paintComponent(Graphics g)
-	{
+	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		// System.out.println("paintComponent");
 		g.drawImage(bufImage, 0, 0, getWidth(), getHeight(), null);
@@ -97,8 +99,7 @@ public class CenterShapesBufferedPanel extends JPanel
 	 * @param width - width of the shape
 	 * @param height - height of the shape
 	 */
-	public void fillOvalCentered(int width, int height)
-	{
+	public void fillOvalCentered(int width, int height) {
 		// left corner X = center of JPanel minus half width of shape
 		int x = this.getWidth() / 2 - width / 2;
 
@@ -115,8 +116,7 @@ public class CenterShapesBufferedPanel extends JPanel
 	 * @param height - height of the shape
 	 * @param g - Graphics object we are drawing on
 	 */
-	public void fillRectCentered(int width, int height)
-	{
+	public void fillRectCentered(int width, int height) {
 		// left corner X = center of JPanel minus half width of shape
 		int x = this.getWidth() / 2 - width / 2;
 
